06e2395dc24b38ebddbdd894987ca58bdde3415d
R-Sama <richardxiao.liu@gmail.com>
2020-08-11 13:23:45 +0100

    did a few exercises...

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,99 @@
+
+/*
+
+
+function calcAv(scoreList) {
+	var totalScore;
+	
+	for (score in scoreList) {
+		totalScore += score;
+	}
+
+	return (totalScore/length(scoreList));
+}
+
+function findWinner(avScoreList) {
+	// 3 vars...
+	// case 1 > 2 && 1 > 3 winner = 1, else if 2 > 1 && 2 > 3 2 = winner, else if... etc
+
+}
+
+var johnTeam, mikeTeam, maryTeam, johnAv, mikeAv, maryAv;
+johnTeam = [89, 120, 103];
+mikeTeam = [116, 94, 123];
+maryTeam = [97, 134, 105];
+
+johnAv = {'John', calcAv(johnTeam)};
+mikeAv = {'Mike', calcAv(mikeTeam)};
+maryAv = {'Mary', calcAv(maryTeam)};
+
+console.log('John\'s team av: ' + johnAv);
+console.log('Mike\'s team av: ' + mikeAv);
+console.log('Mary\'s team av: ' + maryAv);
+
+var foo = function (something) {
+	switch(something) {
+		case: 'yes':
+			return 'no';
+		default:
+			return 'yesno';
+	}
+}
+
+// Arrays
+
+var yes = ['hi', 'yes'];
+//var no = new Array(1,2,3);
+var hi = yes.length;
+yes.push('willAdd');
+yes.unshift('hi');
+yes.pop();
+yes.shift();
+*/
+
+
+/*
+var markMass, johnMass, markHeight, johnHeight, johnBmi, markBmi;
+
+console.log('Let\'s get Mark\'s details first.');
+markMass = getMass();
+markHeight = getHeight();
+console.log('...and now John\'s details.');
+johnMass = getMass();
+johnHeight = getHeight();
+
+
+function getMass() {
+    return prompt('Enter Mass in KG:');
+}
+
+
+function getHeight() {
+    return prompt('Enter height in meters:');
+}
+
+function calcBMI(mass, height) {
+    return (mass/(height*height));
+}
+
+
+var markBMI = calcBMI(markMass, markHeight);
+var johnBMI = calcBMI(johnMass, johnHeight);
+
+console.log(markBMI, + ' ' + johnBMI);
+
+*/
+
+
+//Ternary operator:
+/*
+var age = 22;
+
+age >= 18 ? console.log('beeeeeeeeer') : console.log('jufffice');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+
+*/
+
